22d28679dbc92bb4ca0f1c8007bb3adc3b0cee40
Seon-Dittman <102265624+SeonDittman@users.noreply.github.com>
2023-12-11 16:30:59 -0600

    updated averaging and model

--- ML model/model.py
+++ ML model/model.py
@@ -1,59 +1,45 @@
 import numpy as np
 import os
-os.environ["TF_CCP_MIN_LOG_LEVEL"] = "2"
+import warnings
+os.environ["TF_CCP_MIN_LOG_LEVEL"] = "1"
 import tensorflow as tf
 from tensorflow import keras
-def getResult(currImage):
+
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# warnings.filterwarnings('ignore')
+
+def process_image(currImage):
     #declaring the different classes of possible brain tumor
     class_names=['glioma', 'meningioma', 'notumor', 'pituitary']
 
-    #load model
-    model = keras.models.load_model(r"C:\Users\DittmanSeon\OneDrive - University of Wisconsin-Stout\Documents\GitHub\CS458_Brain_Tumor_Detection\ML model\my_model.keras") #change to new local location of the model
+    #load
+    try:
+        model = keras.models.load_model(r"C:\Users\DittmanSeon\OneDrive - University of Wisconsin-Stout\Documents\GitHub\CS458_Brain_Tumor_Detection\ML model\my_model.keras") #change to new local location of the model
 
-    #create arrays to store percentage confidence
-    glioma=[]
-    meningioma=[]
-    notumor=[]
-    pituitary=[]
+        img_path = currImage  # Full path to the image
+        img = tf.keras.utils.load_img(
+            img_path, target_size=(180, 180)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0) # Create a batch
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+    except Exception:
+        pass
 
-    img_path = currImage  # Full path to the image
-    img = tf.keras.utils.load_img(
-        img_path, target_size=(180, 180)
-    )
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
     #add confidence percentages to designated array
     if (class_names[np.argmax(score)] == "glioma"):
-        glioma.append(100 * np.max(score))
+        glioma = (100 * np.max(score))
+        return (glioma, 1)
     elif (class_names[np.argmax(score)] == "meningioma"):
-        meningioma.append(100 * np.max(score))
+        meningioma = (100 * np.max(score))
+        return (meningioma, 2)
     elif (class_names[np.argmax(score)] == "notumor"):
-        notumor.append(100 * np.max(score))
+        notumor = (100 * np.max(score))
+        return (notumor, 0)
     elif (class_names[np.argmax(score)] == "pituitary"):
-        pituitary.append(100 * np.max(score))
-    #find average percentage of different arrays
-    if (len(glioma) > 0):
-        global g
-        g = sum(glioma) / len(glioma)
-    elif (len(glioma) == 0):
-        g = 0
-    if(len(meningioma) > 0):
-        global m
-        m = sum(meningioma) / len(meningioma)
-    elif (len(meningioma) == 0):
-        m = 0
-    if (len(notumor) > 0):
-        global nt
-        nt = sum(notumor) / len(notumor)
-    elif (len(notumor) == 0):
-        nt = 0
-    if (len(pituitary) > 0):
-        global p
-        p = sum(pituitary) / len(pituitary)
-    elif (len(pituitary) == 0):
-        p = 0
-    #write results to text doc
-    return("Results:glioma- {:.2f} percent confident based on {} images, meningioma- {:.2f} percent confident based on {} images, no tumor- {:.2f} percent confident based on {} images,pituitary- {:.2f} percent confident based on {} images. \n"
-            .format(g,len(glioma),m,len(meningioma),nt,len(notumor),p,len(pituitary)))
+        pituitary = (100 * np.max(score))
+        return (pituitary, 3)
+
